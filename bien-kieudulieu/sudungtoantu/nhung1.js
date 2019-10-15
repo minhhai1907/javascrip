2c13cd8f8903c4641b122c30d4e2a61bf44a0921
minhhai1907 <minhhai1907@gmail.com>
2019-10-15 20:52:23 +0700

    15/10

---
+++ bien-kieudulieu/sudungtoantu/nhung1.js
@@ -0,0 +1,8 @@
+let inputwidth
+        let inputhight
+    inputwidth = prompt ('enter the width')
+    inputhight = prompt ('enter the hight')
+    let withd = parseInt (inputwidth)
+    let hight = parseInt (inputhight)
+    let area = withd * hight
+    document.write ("the area is:" + area)
